32c4d950f4e557ec86e38f045f873b8101e5537e
idosarue <dodojoker67@gmail.com>
2021-07-04 12:37:41 +0300

    xp final

--- week2/day1/xp_exercise/script.js
+++ week2/day1/xp_exercise/script.js
@@ -4,6 +4,7 @@ console.log(5 - "4" ); // 1
 console.log(10 % 5); // 0
 console.log("Java" + "Script"); //"JavaScript"
 console.log(" " + " "); // "  "
+console.log(" " + 0); // " 0"
 console.log(true + true); // 2
 console.log(true + false); // 1
 console.log(false + true); // 1
