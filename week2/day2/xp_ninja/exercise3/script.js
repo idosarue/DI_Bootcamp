1c7a39b88e08247bff926308d3938397d632c488
idosarue <dodojoker67@gmail.com>
2021-07-05 15:18:58 +0300

    finished ninja ex3

---
+++ week2/day2/xp_ninja/exercise3/script.js
@@ -0,0 +1,10 @@
+let userWord = "ahoieuuaaa"
+let a = /[a]/g;
+let e = /[e]/g;
+let i = /[i]/g;
+let o = /[o]/g;
+let u = /[u]/g;
+
+let newW = userWord.replace(a, "1").replace(e, 2).replace(i,3).replace(o, 4).replace(u, 5);
+console.log(newW)
+
