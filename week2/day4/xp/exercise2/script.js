2dce6d083118ffd814052687ab334803ac037de3
idosarue <dodojoker67@gmail.com>
2021-07-06 18:14:31 +0300

    finished ex2

---
+++ week2/day4/xp/exercise2/script.js
@@ -0,0 +1,10 @@
+function checkDriverAge(age){
+    if(age < 18){
+        alert("Sorry, you are too young to drive this car. Powering off");
+    }else if(age > 18){
+        alert("You are old enough to drive, Powering On. Enjoy the ride!");
+    }else{
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+checkDriverAge(19)
