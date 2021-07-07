06e255dcfc8949b230deb724340f7917e0df4e33
idosarue <dodojoker67@gmail.com>
2021-07-07 16:02:14 +0300

    final

--- week2/day4/daily_challenge/script.js
+++ week2/day4/daily_challenge/script.js
@@ -2,8 +2,7 @@
 
 
 function frame() {
-    let space = ' ';
-    let hasComma = true;
+    let space = ' ',hasComma = true;
     while(hasComma){
         let words = prompt("enter strings seperated with comma: ");
         if(!words.includes(",")){
@@ -11,25 +10,23 @@ function frame() {
         }else{
             alert("Finally..")
             hasComma = false;
-            let newWords = words.replace(/\s/g, '').split(",")
-            let star = '*';
-            let dif;
-            let longest = 0;
-            let wordLen;
+            let newWords = words.replaceAll(/\s/g, '').split(",")
+            console.log(words.replaceAll(/\s/g, ''))
+            let star = '*', dif, longest = 0,wordLen;
             for(let i = 0; i< newWords.length; i++){
                 if(newWords[i].length > longest){
                     longest = newWords[i].length
                 }       
             }
-        
-            console.log(star.repeat(longest+5));
+            
+            console.log(star.repeat(longest+4));
         
             for(let i = 0; i < newWords.length ; i++){ 
                 wordLen = newWords[i].length;
                 dif = longest - wordLen;
-                console.log(star, newWords[i],space.repeat(dif), star);
+                console.log(`${star}${space}${newWords[i]}${space.repeat(dif)}${space}${star}`);
             }
-                console.log(star.repeat(longest+5))
+                console.log(star.repeat(longest+4))
             
         }
     }
