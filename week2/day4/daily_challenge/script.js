7473b2ff03382d3dca99eb4f1d46e8ea03d46ee2
idosarue <dodojoker67@gmail.com>
2021-07-07 15:05:20 +0300

    final

--- week2/day4/daily_challenge/script.js
+++ week2/day4/daily_challenge/script.js
@@ -1,33 +1,43 @@
 
 
+
 function frame() {
-    let words = prompt("enter words seperated with comma: ");
-    let newWords = words.replace(/\s/g, '').split(",")
-    console.log(newWords)
-    let star = '*';
     let space = ' ';
-    let dif;
-    let longest = 0;
-    let wordLen ;
-    for(let i = 0; i< newWords.length; i++){
-        console.log(longest);
-        if(newWords[i].length > longest){
-            longest = newWords[i].length
-        }
-        console.log(longest)
+    let hasComma = true;
+    while(hasComma){
+        let words = prompt("enter strings seperated with comma: ");
+        if(!words.includes(",")){
+            alert("missing comma!!")
+        }else{
+            alert("Finally..")
+            hasComma = false;
+            let newWords = words.replace(/\s/g, '').split(",")
+            let star = '*';
+            let dif;
+            let longest = 0;
+            let wordLen;
+            for(let i = 0; i< newWords.length; i++){
+                if(newWords[i].length > longest){
+                    longest = newWords[i].length
+                }       
+            }
         
-    }console.log(longest);
-
-    console.log(star.repeat(longest+5));
-
-    for(let i = 0; i < newWords.length ; i++){ 
-        wordLen = newWords[i].length;
-        dif = longest - wordLen;
-        console.log(star, newWords[i],space.repeat(dif), star);
+            console.log(star.repeat(longest+5));
+        
+            for(let i = 0; i < newWords.length ; i++){ 
+                wordLen = newWords[i].length;
+                dif = longest - wordLen;
+                console.log(star, newWords[i],space.repeat(dif), star);
+            }
+                console.log(star.repeat(longest+5))
+            
+        }
     }
-        console.log(star.repeat(longest+5))
+
 }
-frame();
+
+frame()
+
 
 
 // Hello, World, in, as, frame
