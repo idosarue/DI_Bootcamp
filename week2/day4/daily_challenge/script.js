78b6c99f62b92ee1fa8cc254f0c006a9906028a1
idosarue <dodojoker67@gmail.com>
2021-07-07 13:43:01 +0300

    finished daily4

--- week2/day4/daily_challenge/script.js
+++ week2/day4/daily_challenge/script.js
@@ -1,26 +1,26 @@
 
-
+let words = prompt("enter words seperated with comma: ");
+let newWords = words.replace(/\s+/g, '').split(",")
+let star = '*'
+let space = ' '
+let dif;
+let longest;
 
 function frame(){
-    let words = prompt("enter words seperated with comma: ");
-    let newWords = words.replace(/\s+/g, '').split(",")
-    console.log(newWords)
-    let wordLen = newWords.length + 1;
-    let star = '*'
-    let space = ' '
-    let dif;
-    let long;
-    console.log(star.repeat(long+5))
-    for(let i in newWords){
-        long = Math.max(newWords[i].length)
-        dif = wordLen - long
-        console.log(star, newWords[i],space.repeat(dif), star);
 
+    for(let i = 0; i< newWords.length; i++){
+        longest = Math.max(newWords[i].length) 
     }
+    console.log(star.repeat(longest+5))
 
-        console.log(star.repeat(long+5))
+    for(let i in newWords){ 
+        let wordLen = newWords[i].length;
+        dif = longest - wordLen;
+        console.log(star, newWords[i],space.repeat(dif), star);
+    }
+        console.log(star.repeat(longest+5))
 }
-
 frame();
 
-// Hello, World, in, a, frames
+
+// Hello, World, indd, aasd, frames
