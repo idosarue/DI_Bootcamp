222f88daddcfdc1462e37891799074289fdb13f1
idosarue <dodojoker67@gmail.com>
2021-07-11 14:59:05 +0300

    fixed daily

--- week2/day5/daily_challenge/script.js
+++ week2/day5/daily_challenge/script.js
@@ -1,34 +1,36 @@
-let bottles = 99
-let counter = 1;
-for(let i = 99; i >= 0; i--){
-    let FirstSentenceBottle = `${bottles} of beer on the wall`;
-    let secondSentence = `${bottles} of beer`;
-    let SentenceFor1Bottle = `take ${counter} down pass it around`;
-    let SentenceBottles = `take ${counter} down pass them around`;
-    if(bottles >= 9){
-        if(bottles === 99){
-            bottles -= counter
-            console.log(FirstSentenceBottle)
-            console.log(SentenceFor1Bottle)
-            counter+=1
+let bottles;
+function check(){
+    bottles = +prompt("enter number of bottles: ")
+    while (isNaN(bottles)) {
+        bottles = +prompt("not a number, enter number of bottles: ")
+    }
+}
+function countBottles() {
+    check()
+    let counter = 1;
+    for(let i = bottles; i > 0 && bottles >= 0; i--){
+        console.log( `${bottles} of beer on the wall\n${bottles} of beer`)
+        if(counter < bottles){
+            bottles-=counter    
+            if(counter == 1){
+                console.log(`take ${counter} down pass it around`)
+                console.log( `${bottles} bottles of beer on the wall`)
+            }else if(counter > 1){
+                console.log(`take ${counter} down pass them around`)
+                console.log( `${bottles} bottles of beer on the wall`)
+            }
+        counter+=1
         }else{
-            bottles -= counter
-            console.log(FirstSentenceBottle)
-            console.log(FirstSentenceBottle)
-            console.log(secondSentence)
-            counter +=1
-            console.log(SentenceBottles)
+            counter = bottles
+            if(counter == 1){
+                console.log(`take ${counter} down pass it around`)
+            }else{
+                console.log(`take ${counter} down pass them around`)
+            } 
+            console.log( `${bottles - counter} bottles of beer on the wall`)
+            break
         }
 
-    }else if(bottles == 8){
-        counter = 7
-        console.log(FirstSentenceBottle)
-        console.log(FirstSentenceBottle)
-        console.log()
-        bottles -= 7
-        console.log(`take 7 down pass them around`)
-
-        console.log(`${bottles} bottle of beer on the wall..`)
     }
-
 }
+countBottles()
