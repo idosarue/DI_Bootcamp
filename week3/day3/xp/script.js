1d87ebbdadbe48e59506a2ce3afbc6ba3f3164f2
idosarue <dodojoker67@gmail.com>
2021-07-13 13:13:23 +0300

    initial

---
+++ week3/day3/xp/script.js
@@ -0,0 +1,19 @@
+let id;
+let pos = 0;
+function myMove() {
+  let box = document.getElementById("myAnimation");   
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      box.style.left = pos + 'px'; 
+    }
+  }
+}
+
+function stop() {
+  clearInterval(id);
+    
+}
