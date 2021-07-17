927c23619f8884be69a2b3f82f239b3363cb8650
idosarue <dodojoker67@gmail.com>
2021-07-17 18:31:19 +0300

    added js

---
+++ week1/day5/PortfolioProject/script.js
@@ -0,0 +1,13 @@
+let button = document.getElementById('contact');
+
+button.addEventListener('click',function(){
+    window.open("mailto:dodojoker67@gmail.com");
+
+})
+
+let button2 = document.getElementById('btn');;
+
+button2.addEventListener('click',function(){
+    window.open("https://github.com/idosarue")
+
+})
